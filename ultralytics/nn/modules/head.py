09a7410b127b7b4233a2f7cd4e047c5e8116675f
LQD1202 <lqd1202@example.com>
2025-11-13 13:59:46 +0700

    13112025

--- ultralytics/nn/modules/head.py
+++ ultralytics/nn/modules/head.py
@@ -1244,19 +1244,25 @@ class HeatmapAttention(Detect):
         self.cv4 = nn.ModuleList(nn.Sequential(Conv(x, c4, 3, 2, 1), 
                                                Conv(c4, c4, 3, 2, 1), 
                                                AAttn(c4, 8, 1), 
-                                               AAttn(c4, 8, 1), 
-                                               nn.Upsample(scale_factor=2),
-                                               Conv(c4, c4, 3), 
-                                               nn.Upsample(scale_factor=2),
-                                               Conv(c4, c4, 3), 
-                                               nn.Conv2d(c4, 1, 1)) for x in ch)
+                                               AAttn(c4, 8, 1)) for x in ch)
+        
+        self.cv4_2 = Conv(c4, c4, 3)
+        self.cv4_3 = Conv(c4, c4, 3)
+        self.cv4_4 = nn.Conv2d(c4, 1, 1)
 
 
     def forward(self, x: list[torch.Tensor]) -> tuple | list[torch.Tensor]:
         bs = x[0].shape[0]  # batch size
         heatmaps = [self.cv4[i](x[i]) for i in range(self.nl)]
 
-        x_attentions = [_x * hm for _x, hm in zip(x, heatmaps)]
+        x_attentions = []
+        for _x, hm in zip(x, heatmaps):
+            if _x.shape[-1] != hm.shape[-1]:
+                h, w = _x.shape[2:]
+                hm = F.upsample(hm, size = (h, w))
+            x_attentions.append(_x*hm)
+
+        # x_attentions = [_x * hm for _x, hm in zip(x, heatmaps)]
 
         x = Detect.forward(self, x_attentions)
         if self.training:
